f1fcff6762e02f875e589c2fd38e309d9e638835
box-12q <grigorijbockarev55@gmail.com>
2025-12-10 22:14:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -158,4 +158,4 @@ if __name__ == '__main__':
     print("=" * 50)
     print("🔍 Анализатор полярных шапок Марса")
     print("=" * 50)
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
