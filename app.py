a3127406957b839af396d491362969b153cf2d72
box-12q <grigorijbockarev55@gmail.com>
2026-01-30 09:56:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -172,6 +172,6 @@ def show_result(index):
 if __name__ == '__main__':
     print("=" * 50)
     print("🔍 Мульти-анализатор полярных шапок Марса запущен")
-    print("http://127.0.0.1:5000")
     print("=" * 50)
-    app.run(debug=True, host='127.0.0.1', port=5000)
+
+    app.run(debug=True, host='0.0.0.0', port=5000)
